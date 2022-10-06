6209cfe59cc30c32ecb459cff461522f509b77da
LipeART <49559809+LipeART@users.noreply.github.com>
2022-10-06 19:57:34 -0300

    Atualização na interface

--- testeInterface.py
+++ testeInterface.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from tkinter import simpledialog
@@ -8,7 +9,7 @@ import sys
 interface = Tk()
 interface.title("FaceRecon")#Nome do programa
 #tamanho em pixels da janela inicial
-interface.geometry("1500x1080")
+interface.geometry("800x600")
 interface.resizable(False, False)
 #backGround (bg) color
 interface['bg'] = "#aaaaaa"
@@ -19,6 +20,7 @@ options['defaultextension'] = '.jpeg, .png'
 options['initialfile'] = '*.jpeg, *.png'
 
 class Operacoes:
+    
     def importImage(self):
         filename = filedialog.askopenfilename()
         global image
@@ -26,7 +28,15 @@ class Operacoes:
             image = Image.open(filename)
             image = image.resize((250,250))
             image = ImageTk.PhotoImage(image)
-            interface.janelaImagem.create_image(450, 300, image=image)
+            interface.janelaImagem.create_image(200, 200, image=image)
+            interface.textoMsg.delete('1.0', tk.END)           
+            interface.textoMsg.insert('1.0', "Imagem carregada com sucesso")
+        else:
+            interface.textoMsg.delete('1.0', tk.END)           
+            interface.textoMsg.insert('1.0', "Erro ao carregar a imagem")
+    def classificador(self):
+        interface.textoMsg.delete('1.0', tk.END)           
+        interface.textoMsg.insert('1.0', "Funcionalidade em desenvolvimento")
 
 #Interface do menu. Cada botão executa uma tarefa
 class Application(Frame):
@@ -34,25 +44,27 @@ class Application(Frame):
         Frame.__init__(self, master)
         operacoes = Operacoes()
 
-        adicionar = Button(interface, text="Adicionar imagem", padx=10, pady=10, border=2, command=operacoes.importImage)
+        frameBotoes = Frame(interface, width=800, height=30)
+        frameBotoes.grid(row=0, column=0)
+
+        adicionar = Button(frameBotoes, text="Adicionar imagem", padx=10, pady=10, border=2, command=operacoes.importImage)
         adicionar.grid(row=0, column=0)
-        frameBotoes = Frame(interface, width=600, height=30)
-        frameBotoes.grid(row=0, column=1)
-        btnTeste1 = Button(frameBotoes, text="Classificador", padx=10, pady=10, border=2)
-        btnTeste1.grid(row=0, column=0)
+        
+        btnTeste1 = Button(frameBotoes, text="Classificador", padx=10, pady=10, border=2, command=operacoes.classificador)
+        btnTeste1.grid(row=0, column=1)
         #btnTeste2 = Button(frameBotoes, text="Teste 2", padx=10, pady=10, border=2)
         #btnTeste2.grid(row=0, column=1)
         #btnTeste3 = Button(frameBotoes, text="Teste 3", padx=10, pady=10, border=2)
         #btnTeste3.grid(row=0, column=2)
         #btnTeste4 = Button(frameBotoes, text="Teste 4", padx=10, pady=10, border=2)
         #btnTeste4.grid(row=0, column=3)
-        self.janelaImagem = Canvas(interface, width=900, height=720, bg="#999999")
+        self.janelaImagem = Canvas(interface, width=400, height=600, bg="#999999")
         self.janelaImagem.grid(row=1, column=0)
-        janelaMsg = Frame(interface, width=500, height=720, bg="#eeeeee")
+        janelaMsg = Frame(interface, width=400, height=600, bg="#eeeeee")
         janelaMsg.grid(row=1, column=1)
-        textoMsg = Text(janelaMsg, height=12)
-        textoMsg['state'] = 'disabled'
-        textoMsg.insert('1.0', "Olá, aqui vai aparecer a mensagem que tiver que aparecer :)")
+        self.textoMsg = Text(janelaMsg)
+        self.textoMsg.grid(row=0,column=0)
+
         #adicionar.pack()
         #self.pack()
 
