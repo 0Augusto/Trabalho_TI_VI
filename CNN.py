9a31fbb9f8ca88afc22abbebf2213cef1c69f806
Henrique Augusto <73003917+0Augusto@users.noreply.github.com>
2022-11-02 15:44:41 -0300

    Add files via upload
    Modelo + treinamento com AlexNet

--- CNN.py
+++ CNN.py
@@ -63,3 +63,39 @@ class CNN (object):
         model.add(BatchNormalization())
         model.add(Activation('relu'))
         model.add(Conv2D(256, (3, 3), strides=(1, 1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(4096))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(4096))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        
+        #modelo de treinamento reconhecimento de faces usando o modelo de treinamento do AlexNet
+        model.add(Dense(self.num_classes, activation='softmax'))
+        
+        #model.summary()
+         
+        # Training
+        sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+        checkpointer = ModelCheckpoint(filepath=self.model_path, verbose=1, save_best_only=True)
+        model.fit_generator( train_generator, steps_per_epoch=2000 // self.batch_size, epochs=self.epochs, validation_data=validation_generator, validation_steps=800 // self.batch_size, callbacks=[checkpointer])
+         
+        # Save model
+        model.save(self.model_path)
+        
+    def predict(self, img_path):
+        model = keras.models.load_model(self.model_path)
+        img = image.load_img(img_path, target_size=(self.img_width, self.img_height))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        preds = model.predict(x)
+        return preds
+    
