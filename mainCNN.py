681bb5d6e24614ef79156552f6b3336d6fc3b2fc
dannielHCVieira <danniel.hcvieira@gmail.com>
2022-11-10 17:20:32 -0300

    Added stats fixed

--- mainCNN.py
+++ mainCNN.py
@@ -2,5 +2,6 @@ import CNN as cnn
 
 if __name__ == "__main__":
     cnn_test = cnn.CNN("./dataset/",250,250,32,14,6,"./dataset/output/model/")
-    cnn_test.train()
+    #cnn_test.clean()
+    #cnn_test.train()
     cnn_test.get_stats()
